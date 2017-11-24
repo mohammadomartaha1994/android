905f8ee959053fd124bc6b9be83d98615560fee4
mohammadomartaha1994 <32689856+mohammadomartaha1994@users.noreply.github.com>
2017-11-25 00:28:07 +0200

    Update and rename a to use.java

---
+++ Session/use.java
@@ -0,0 +1,17 @@
+//set 
+    
+        Session.setUsername(username);
+        Session.setID(user_id);
+        Session.setage(age);
+        Session.setname(name);
+        Session.setphoto(photo);
+        Session.setphone(phone);
+
+
+//get
+     
+        String currentUserName=Session.getUsername();
+        int currentID=Session.getID();
+
+
+
