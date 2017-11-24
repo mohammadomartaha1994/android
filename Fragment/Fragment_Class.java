51776bf76fc8905726e60629ccfea374bc3a820d
mohammadomartaha1994 <32689856+mohammadomartaha1994@users.noreply.github.com>
2017-11-25 00:40:49 +0200

    Update and rename Num_Three.java to Fragment_Class.java

---
+++ Fragment/Fragment_Class.java
@@ -0,0 +1,18 @@
+public class Fragment_Class extends Fragment implements View.OnClickListener {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.num_three, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+    @Override
+    public void onClick(View v) {
+
+    }
+
+}
