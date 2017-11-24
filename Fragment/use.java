4e04162f16a2eb7223fdbb8a5a19ea452c7ea58b
mohammadomartaha1994 <32689856+mohammadomartaha1994@users.noreply.github.com>
2017-11-25 00:45:05 +0200

    Update use.java

--- Fragment/use.java
+++ Fragment/use.java
@@ -1,2 +1,11 @@
+        create class name Fragment_Class 
+                and make in extentds Fragment
+                
+         then make layout name fragment_layout 
+                and give it ID 
+
+
+        Call it :        
+
         Fragment_Class fragment = new Fragment_Class(); // to fragemnt class
         getFragmentManager().beginTransaction().replace(R.id.content, fragment).commit();  /// from the layout you need to move 
