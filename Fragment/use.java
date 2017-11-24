8df1182bd13d6d646ffab55867485a749dae3953
mohammadomartaha1994 <32689856+mohammadomartaha1994@users.noreply.github.com>
2017-11-25 00:38:17 +0200

    Update use.java

--- Fragment/use.java
+++ Fragment/use.java
@@ -1,1 +1,2 @@
-sds
+        Fragment_Class fragment = new Fragment_Class(); // to fragemnt class
+        getFragmentManager().beginTransaction().replace(R.id.content, fragment).commit();  /// from the layout you need to move 
